5c353d55313b1e520ad1b68c01d1b2bcbfd7b77e
AndresFernandoVargasTabaresADSO <anonymatusrs@gmail.com>
2022-09-20 19:24:52 -0500

    Ejercicio 5

--- jsjs.js
+++ jsjs.js
@@ -1,23 +1,15 @@
 addEventListener("DOMContentLoaded",()=>{
-    console.log(`4. Cálculo de áreas - Elige una figura geométrica:" Triángulo y Círculo
-    ¿Qué figura quiere calcular (Escriba T o C)?
-    Triangulo = base * altura / 2
-    Circulo = PI * radio* radio`)
-   
-    let options = String(prompt(`Ingresa la Opcion de la figura que deas usar, Opciones: Trianguno (T) - Circulo (C): `))
+    console.log(`5. Escriba un algoritmo que calcule el área de un cuadrado siempre y cuando los lados sean positivos:
+    área triangulo= lado * lado.`)
 
-    if(options==`T`){
-        let base = Number(prompt(`Ingresa la base de tu triangulo: `))
-        let altura = Number(prompt(`Ingresa la altura de tu triangulo: `))
+    let lado = Number(prompt(`Ingresa el lado del cuadrado: `))
 
-        let solucion = (base * altura)/2
-        console.log(`El area de tu triangulo es: ${solucion}`)
+    if(lado>0){
+        let solucion = lado*lado
+        console.log(`El area de tu cuadrado es: ${solucion}`)
     }
 
     else{
-        let radio = Number(prompt(`Ingresa el radio de tu circulo: `))
-
-        let solucion = (Math.PI * Math.pow(radio,2))
-        console.log(`El area de tu circuferencia es: ${solucion}`)
+        console.log(`Ingresa un numero positivo!!`)
     }
 })
