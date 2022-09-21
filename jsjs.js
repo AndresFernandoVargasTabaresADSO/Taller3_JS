334f898ca2606cec01b3a13ab908cc60574cb7c5
AndresFernandoVargasTabaresADSO <anonymatusrs@gmail.com>
2022-09-20 19:03:29 -0500

    Ejercicio 3

--- jsjs.js
+++ jsjs.js
@@ -1,19 +1,47 @@
 addEventListener("DOMContentLoaded",()=>{
-    console.log(`2. Calcule el mayor de tres números, permitiendo leer 3 valores diferentes`)
+    console.log(`3. Desarrollar un diagrama que lea 3 valores diferentes e indique cual es el mayor de ellos, el menor o
+    si son iguales.`)
     
     let number = Number(prompt(`Escribe el primer numero: `))
-    let numbertwo = Number(prompt(`Escribe el segundo numero: `))
+    let numbertwo = Number(prompt(`Escribe un segundo numero: `))
     let numbertre = Number(prompt(`Escribe el tercer numero: `))
 
-    if(number<numbertwo,numbertre<numbertwo){
+    if(number==numbertwo|number==numbertre|numbertwo==numbertre|numbertwo==number){
+        if(numbertwo==number){
+            console.log(`El primer numero es igual: ${numbertwo} Es igual al segundo numero: ${number}`)
+        }
+
+        if(numbertre==number){
+            console.log(`El primer numero:${numbertre} Es igual al tercer numero ${number}`)
+        }
+
+        if(numbertwo==numbertre){
+            console.log(`El segundo numero:${numbertre} Es igual al tercer numero ${number}`)
+        }
+    }
+
+    if(number<numbertwo&numbertre<numbertwo){
         console.log(`${numbertwo} Es mayor que ${number} y ${numbertre}`)
     }
-
-    if(numbertwo<number,numbertre<number){
+        
+    if(numbertwo<number&numbertre<number){
         console.log(`${number} Es mayor que ${numbertwo} y ${numbertre}`)
     }
-
-    if(number<numbertre,numbertwo<numbertre){
+        
+    if(number<numbertre&numbertwo<numbertre){
         console.log(`${numbertre} Es mayor que ${number} y ${numbertwo}`)
     }
+        
+    if(number>numbertwo&numbertre>numbertwo){
+        console.log(`${numbertwo} Es menor que ${number} y ${numbertre}`)
+    }
+        
+    if(numbertwo>number&numbertre>number){
+        console.log(`${number} Es menor que ${numbertwo} y ${numbertre}`)
+    }
+        
+    if(number>numbertre&numbertwo>numbertre){
+        console.log(`${numbertre} Es menor que ${number} y ${numbertwo}`)
+    }
+    
 })
