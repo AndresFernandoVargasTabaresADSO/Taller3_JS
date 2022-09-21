49df3b886d6c671dad720dbd1662bb1bd780fda1
AndresFernandoVargasTabaresADSO <anonymatusrs@gmail.com>
2022-09-20 22:11:25 -0500

    Ejercicio 11

--- jsjs.js
+++ jsjs.js
@@ -1,30 +1,27 @@
 addEventListener("DOMContentLoaded",()=>{
-    console.log(`10. Calcular el valor total del valor de 5 productos, el IVA y el subtotal, visualizar los resultados de:
-    Iva, Subtotal y Total de la compra de los artículos.`)
+    console.log(`11. Programa para solicitar el nombre, apellido, edad y la nota promedio de 5 estudiantes de un curso
+    de computación.`)
 
-    var continuar = `si`
+    var cont=0
+    var n=0
+    
+    let nombre= String(prompt(`Ingresa tu nombre y apellido: `))
+    let edad= Number(prompt(`Ingresa tu edad: `))
+    var continuar= `si`
     while(continuar==`si`){
-        let p1 = Number(prompt(`Ingresa el valor del producto 1: `))
-        let p2 = Number(prompt(`Ingresa el valor del producto 2: `))
-        let p3 = Number(prompt(`Ingresa el valor del producto 3: `))
-        let p4 = Number(prompt(`Ingresa el valor del producto 4: `))
-        let p5 = Number(prompt(`Ingresa el valor del producto 5: `))
-        let valoriva = Number(prompt(`Ingresa el valor del IVA: `))
-        continuar = String(prompt(`Deas continuar? (si/no)`))
+        let nota = Number(prompt(`Ingresa la notas: `))
+        cont=cont+nota
+        continuar= String(prompt(`Deseas agregar mas notas? (si/no)`))
+        if(nota>0){
+            n = n + 1
+        }
         
-        let suma = (p1+p2+p3+p4+p5)
-        let iva = (suma * valoriva/100)
-        let total = (suma + iva) 
-        console.log(`Valor sin iva ${suma}`)
-        console.log(`IVA ${valoriva}%`)
-        console.log(`Valor del iva ${valoriva} = ${iva}`)
-        console.log(`Valor con iva: ${total}`)
-    }
-
-    if(continuar==`no`){
-        console.log(`Gracias por su compra!`)
-    }
-    else{
-        console.log(`Ingreso un dato no valido!`)
+        if(continuar==`no`){
+            let promedio = cont/n
+            console.log(`Estudiantes ${nombre}`)
+            console.log(`Edad: ${edad}`)
+            console.log(`Tu promedio es de: ${promedio}`)
+        }
+        
     }
 })
